f098ca680f1ac07fd0d0f0a9a696cb774eb7d0da
Javier Arellano <franjarca@hotmail.com>
2018-01-10 13:01:03 +0100

    added Mysql.Data

--- Agenda/Form1.cs
+++ Agenda/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace Agenda {
     public partial class Form1 : Form {
@@ -18,6 +19,18 @@ namespace Agenda {
 
         }
 
+        public String CrearCadenaConexion() {
+            MySqlConnectionStringBuilder builder = new MySqlConnectionStringBuilder();
+            builder.Server = "localhost";
+            builder.UserID = "java";
+            builder.Password = "java";
+            builder.Database = "interfaz";
+            return builder.ToString();
+        }
+        public MySqlConnection conectar() {
+            MySqlConnection conn = new MySqlConnection(CrearCadenaConexion());
+            return conn;
+        }
     }
 
     public class Contacto
