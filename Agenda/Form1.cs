26fdf9818eb5823443296d352f6ae79bebcec92f
Alex <alconpla@gmail.com>
2018-01-10 12:59:28 +0100

    3

--- Agenda/Form1.cs
+++ Agenda/Form1.cs
@@ -7,11 +7,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using MySql.Data.MySqlClient;
 
 namespace Agenda {
     public partial class Form1 : Form {
-        List<Contacto> usuarios = new List<Contacto>();
         public Form1() {
             InitializeComponent();
         }
@@ -19,25 +17,20 @@ namespace Agenda {
         private void button1_Click(object sender, EventArgs e) {
 
         }
-        public String CrearCadenaConexion() {
-            MySqlConnectionStringBuilder builder = new MySqlConnectionStringBuilder();
-            builder.Server = "localhost";
-            builder.UserID = "java";
-            builder.Password = "java";
-            builder.Database = "interfaz";
-            return builder.ToString();
-        }
-        public MySqlConnection conectar() {
-            MySqlConnection conn = new MySqlConnection(CrearCadenaConexion());
-            return conn;
-        }
+
     }
 
     public class Contacto
     {
         public Contacto(int id, string nombre, int telefono, string direccion, string email)
         {
-            //a
+            this.id = id;
+            this.nombre = nombre;
+            this.telefono = telefono;
+            this.direccion = direccion;
+            this.email = email;
+
+        
         }
 
     }
