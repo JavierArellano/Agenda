0bc7d55593f76443e844040eaa504d764989cfb1
Javier Arellano <franjarca@hotmail.com>
2018-01-11 08:45:17 +0100

    algo he hecho

--- Agenda/Form1.Designer.cs
+++ Agenda/Form1.Designer.cs
@@ -47,10 +47,10 @@
             // 
             // bMostrar
             // 
-            this.bMostrar.Location = new System.Drawing.Point(350, 386);
-            this.bMostrar.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.bMostrar.Location = new System.Drawing.Point(467, 475);
+            this.bMostrar.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.bMostrar.Name = "bMostrar";
-            this.bMostrar.Size = new System.Drawing.Size(156, 39);
+            this.bMostrar.Size = new System.Drawing.Size(208, 48);
             this.bMostrar.TabIndex = 0;
             this.bMostrar.Text = "Mostrar";
             this.bMostrar.UseVisualStyleBackColor = true;
@@ -58,10 +58,10 @@
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(22, 25);
-            this.dataGridView1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.dataGridView1.Location = new System.Drawing.Point(29, 31);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(790, 331);
+            this.dataGridView1.Size = new System.Drawing.Size(1053, 407);
             this.dataGridView1.TabIndex = 1;
             // 
             // tabControl1
@@ -71,10 +71,11 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.tabControl1.Controls.Add(this.tabPage1);
             this.tabControl1.Controls.Add(this.tabPage2);
-            this.tabControl1.Location = new System.Drawing.Point(0, 6);
+            this.tabControl1.Location = new System.Drawing.Point(0, 7);
+            this.tabControl1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(850, 481);
+            this.tabControl1.Size = new System.Drawing.Size(1133, 592);
             this.tabControl1.TabIndex = 13;
             // 
             // tabPage1
@@ -82,10 +83,11 @@
             this.tabPage1.BackColor = System.Drawing.Color.MediumSeaGreen;
             this.tabPage1.Controls.Add(this.dataGridView1);
             this.tabPage1.Controls.Add(this.bMostrar);
-            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Location = new System.Drawing.Point(4, 25);
+            this.tabPage1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.tabPage1.Name = "tabPage1";
-            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(842, 455);
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tabPage1.Size = new System.Drawing.Size(1125, 563);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "Mostrar datos";
             // 
@@ -101,10 +103,11 @@
             this.tabPage2.Controls.Add(this.tEmail);
             this.tabPage2.Controls.Add(this.tNombre);
             this.tabPage2.Controls.Add(this.tTelefono);
-            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Location = new System.Drawing.Point(4, 25);
+            this.tabPage2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.tabPage2.Name = "tabPage2";
-            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(842, 455);
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tabPage2.Size = new System.Drawing.Size(1125, 563);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "Operaciones";
             this.tabPage2.UseVisualStyleBackColor = true;
@@ -112,18 +115,20 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(38, 150);
+            this.label5.Location = new System.Drawing.Point(51, 185);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(32, 13);
+            this.label5.Size = new System.Drawing.Size(42, 17);
             this.label5.TabIndex = 22;
             this.label5.Text = "Email";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(30, 124);
+            this.label4.Location = new System.Drawing.Point(40, 153);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(52, 13);
+            this.label4.Size = new System.Drawing.Size(67, 17);
             this.label4.TabIndex = 21;
             this.label4.Text = "Direccion";
             // 
@@ -131,26 +136,29 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.25F);
-            this.label1.Location = new System.Drawing.Point(45, 32);
+            this.label1.Location = new System.Drawing.Point(60, 39);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(162, 25);
+            this.label1.Size = new System.Drawing.Size(190, 30);
             this.label1.TabIndex = 18;
             this.label1.Text = "Añadir contacto";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(30, 98);
+            this.label3.Location = new System.Drawing.Point(40, 121);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(49, 13);
+            this.label3.Size = new System.Drawing.Size(64, 17);
             this.label3.TabIndex = 20;
             this.label3.Text = "Telefono";
             // 
             // insertar
             // 
-            this.insertar.Location = new System.Drawing.Point(75, 191);
+            this.insertar.Location = new System.Drawing.Point(100, 235);
+            this.insertar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.insertar.Name = "insertar";
-            this.insertar.Size = new System.Drawing.Size(112, 39);
+            this.insertar.Size = new System.Drawing.Size(149, 48);
             this.insertar.TabIndex = 13;
             this.insertar.Text = "Insertar";
             this.insertar.UseVisualStyleBackColor = true;
@@ -158,48 +166,53 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(35, 75);
+            this.label2.Location = new System.Drawing.Point(47, 92);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.Size = new System.Drawing.Size(58, 17);
             this.label2.TabIndex = 19;
             this.label2.Text = "Nombre";
             // 
             // tDireccion
             // 
-            this.tDireccion.Location = new System.Drawing.Point(88, 124);
+            this.tDireccion.Location = new System.Drawing.Point(117, 153);
+            this.tDireccion.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.tDireccion.Name = "tDireccion";
-            this.tDireccion.Size = new System.Drawing.Size(134, 20);
-            this.tDireccion.TabIndex = 14;
+            this.tDireccion.Size = new System.Drawing.Size(177, 22);
+            this.tDireccion.TabIndex = 3;
             // 
             // tEmail
             // 
-            this.tEmail.Location = new System.Drawing.Point(88, 150);
+            this.tEmail.Location = new System.Drawing.Point(117, 185);
+            this.tEmail.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.tEmail.Name = "tEmail";
-            this.tEmail.Size = new System.Drawing.Size(134, 20);
-            this.tEmail.TabIndex = 15;
+            this.tEmail.Size = new System.Drawing.Size(177, 22);
+            this.tEmail.TabIndex = 4;
             // 
             // tNombre
             // 
-            this.tNombre.Location = new System.Drawing.Point(88, 72);
+            this.tNombre.Location = new System.Drawing.Point(117, 89);
+            this.tNombre.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.tNombre.Name = "tNombre";
-            this.tNombre.Size = new System.Drawing.Size(134, 20);
-            this.tNombre.TabIndex = 17;
+            this.tNombre.Size = new System.Drawing.Size(177, 22);
+            this.tNombre.TabIndex = 1;
             // 
             // tTelefono
             // 
-            this.tTelefono.Location = new System.Drawing.Point(88, 98);
+            this.tTelefono.Location = new System.Drawing.Point(117, 121);
+            this.tTelefono.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.tTelefono.Name = "tTelefono";
-            this.tTelefono.Size = new System.Drawing.Size(134, 20);
-            this.tTelefono.TabIndex = 16;
+            this.tTelefono.Size = new System.Drawing.Size(177, 22);
+            this.tTelefono.TabIndex = 2;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(849, 488);
+            this.ClientSize = new System.Drawing.Size(1132, 601);
             this.Controls.Add(this.tabControl1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Form1";
             this.Text = "Agenda";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
